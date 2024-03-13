2c2036071fcab3a7d51869e0c893710138fed5ef
MRxEPIC <161140403+MRxEPIC@users.noreply.github.com>
2024-03-13 22:41:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanjutyson:sanjutysonn@ty
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001773149136"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002146422176"))
 # Auto Delete Information
 AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "300")) # Time in Seconds
 
